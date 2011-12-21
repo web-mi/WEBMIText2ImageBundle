3c3cf0a14475940c1c4252d1dbf2b44b03e1289a
prog_standard <prog_standard@10.10.200.134>
2011-12-21 11:33:26 +0100

    umbau

--- Twig/Extension/Text2ImageTwigExtension.php
+++ Twig/Extension/Text2ImageTwigExtension.php
@@ -10,11 +10,16 @@ class Text2ImageTwigExtension extends \Twig_Extension
         return array(
             'var_dump'   => new \Twig_Filter_Function('var_dump'),
             'highlight'  => new \Twig_Filter_Method($this, 'highlight'),
+            'text2image'  => new \Twig_Filter_Method($this, 'highlight'),
         );
     }
 
     public function highlight($sentence, $expr) {
         return preg_replace('/(' . $expr . ')/', '<span style="color:red">\1</span>', $sentence);
+    }
+    
+    public function text2image($sentence, $expr) {
+        return preg_replace('/(' . $expr['i'] . ')/', '<span style="color:red">\1</span>', $sentence);
     }
 
     public function getName()
